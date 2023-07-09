aed24856d66b0d2c35a132b7e12e31eb0097f60a
Nicolas <nicolasmarcope@gmail.com>
2023-07-09 20:45:21 +0200

    create users model and api

---
+++ app/user/serializers.py
@@ -0,0 +1,69 @@
+"""Serializsers for user app."""
+
+from django.contrib.auth import get_user_model, authenticate # authenticate is a helper function that comes with django
+# serializer is a way of converting data into a format that can be easily transferred over the network
+from rest_framework import serializers
+from django.utils.translation import gettext_lazy as _ # this is a convention for converting strings to human readable text
+
+
+# model serializer is a serializer that is specifically tied to a model in the database
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer for the users object."""
+    # the model and the additional fields we want to make accessible to the serializer
+    class Meta:
+        model = get_user_model()
+        fields = ('email', 'password', 'name')
+        extra_kwargs = {'password': {'write_only': True,
+                                     'min_length': 5}}
+
+    # override the create function to create a user with an encrypted password
+    def create(self, validated_data):
+        """Create a new user with encrypted password and return it."""
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it."""
+        # remove the password from the validated data
+        password = validated_data.pop('password', None)
+        # call the update function on the model
+        user = super().update(instance, validated_data)
+
+        # if the password exists then we set the password
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for the user authentication object."""
+    # this is the email and password that will be used to authenticate the user
+    email = serializers.CharField()
+    password = serializers.CharField(
+        style={'input_type': 'password'},
+        trim_whitespace=False # this is to make sure that the password is not trimmed
+    )
+
+    # this is the function that validates the data that is passed in
+    def validate(self, attrs):
+        """Validate and authenticate the user."""
+        # attrs is the data that is passed in
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        # this is the function that will authenticate the user
+        user = authenticate(
+            # this is the context that is passed in
+            request=self.context.get('request'),
+            username=email,
+            password=password
+        )
+        # if the authentication fails then we raise an error
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        # this is the attrs that will be returned
+        attrs['user'] = user
+        return attrs
---
+++ app/user/views.py
@@ -0,0 +1,39 @@
+""""
+Views for the user api
+"""
+# handles the logic for requests and responses
+from rest_framework import generics, authentication, permissions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from user.serializers import UserSerializer, AuthTokenSerializer
+
+
+# handles http post requests, only need to define the serializer and set it
+# here and it will automatically create the view for us
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for the user."""
+    # this is the serializer that will be used to generate the token
+    serializer_class = AuthTokenSerializer
+    # this is the renderer that will be used to render the view
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+    # this is the serializer that will be used to generate the token
+    serializer_class = UserSerializer
+    # this is the authentication class that will be used
+    authentication_classes = (authentication.TokenAuthentication,)
+    # this is the permission class that will be used
+    permission_classes = (permissions.IsAuthenticated,)
+
+    # this is the function that will retrieve the user that is authenticated
+    def get_object(self):
+        """Retrieve and return authenticated user."""
+        # the user is going to be attached to the request
+        return self.request.user
